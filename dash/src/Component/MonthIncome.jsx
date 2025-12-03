90d46036f62d74ec1aea9b516a9d14823b8bc42e
mariam-ship-it118 <22-101170@students.eui.edu.eg>
2025-12-03 22:41:10 +0200

    feat: home section one completed

---
+++ dash/src/Component/MonthIncome.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import './MonthIncome.css';
+
+const MonthIncome = (props) => {
+    return ( 
+        <>
+        
+        <section className='textincome'>
+            <p>{props.month}</p>
+             <p>{props.salary}</p>
+        </section>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        </>
+     );
+}
+ 
+export default MonthIncome;
--- dash/src/Pages/HomeDash.jsx
+++ dash/src/Pages/HomeDash.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import SideBar from '../Component/SideBar';
 import HeadSection from '../Component/HeadSection';
-
+import './Home.css';
+import MonthIncome from '../Component/MonthIncome';
 const HomeDash = () => {
     return ( 
         <>
@@ -9,6 +10,52 @@ const HomeDash = () => {
         
         <SideBar />
         <HeadSection greetings='Welcome' moi='Mariam!' />
+
+<section className='two-cards'>
+
+
+<section className='card-one'>
+    <h3 className='cards-titles'>Add Project</h3>
+    <button className='card-btn'>New</button>
+</section>
+
+  <section className='card-two'>
+    <h3 className='cards-titles'>CV</h3>
+    <section className='btn-div'>
+        <button className='card-btn'>Upload</button>
+       <button className='card-btn'>Download</button>
+    </section>
+  </section>
+
+
+</section>
+
+ 
+ <section className='income'>
+   <h3 className='cards-titles'>Monthly income</h3>
+
+ <MonthIncome month='November' salary='20,000' />
+
+  
+   <MonthIncome month='November' salary='50,000' />
+<MonthIncome month='October' salary='20,000' />
+
+<MonthIncome month='September' salary='70,000' />
+<MonthIncome month='August' salary='100,000' />
+
+ </section>
+
+
+
+
+
+
+
+
+
+
+
+
         </>
      );
 }
